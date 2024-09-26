f839baa2cafe71a5b53b235480ba0c8b915a90dd
evapacheco142 <evasandridpachecodelacruz0@gmail.com>
2024-09-25 22:41:13 -0500

    Templates modificados para mostrar post desde base de datos.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -10,10 +7,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
